d139b3a94df8db7760d6f386894e26ca7bf08794
Karen Rebeca Ortiz ✨ <Karen.ortizg@yahoo.com>
2025-10-02 13:29:56 -0600

    Vercel Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import lenis from 'astro-lenis';
-
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   output: 'server',
+  adapter: vercel(),
   integrations: [lenis(), react()],
   markdown: {
     shikiConfig: {
