e12e7d99a496683a6ce1887f39da503d232da8e9
Karen Rebeca Ortiz ✨ <Karen.ortizg@yahoo.com>
2025-10-02 00:43:53 -0600

    Services and Projects added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import lenis from 'astro-lenis';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   output: 'static',
-  integrations: [lenis()],
+  integrations: [lenis(), react()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
